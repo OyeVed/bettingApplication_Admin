39604bbc0e7be1c53aa33fa15a7d8f8eb0023817
Ayush Verma | Oyesters <ayush@oyesters.in>
2022-09-13 18:26:32 +0530

    Error handling in result_date

--- backend/views/markets/results/result_date.php
+++ backend/views/markets/results/result_date.php
@@ -25,41 +25,38 @@ if(auth($token)){
     $query = $con -> prepare($sql);
     $query->bindparam(":market_id", $market_id, PDO::PARAM_STR);
 
-    if(!$query->execute()){
-        $status = 203;
-        $response = [
-            "msg" => "Results can't be fetched"
-        ];
-        return;   
-    }
-    
-    if($query->rowCount() != 0) {
-
+    if(!$query->execute() || $query->rowCount() == 0){
         //taking previous result date and morkaet in days from market table
+        $sql = "SELECT market_on_days FROM market_table WHERE market_id = :market_id";
+        $query = $con -> prepare($sql);
+        $query->bindparam(":market_id", $market_id, PDO::PARAM_STR);
+        $query->execute();
+        
         $info = $query->fetchAll(PDO::FETCH_ASSOC)[0];
-        $date = $info['date'];
         $market_on_days = $info['market_on_days'];
         $market_on_days = explode(",", $market_on_days);
 
-        //finding next available date 
-        do{
-            $date = date('Y-m-d', strtotime($date . '+1 day'));
-            $next_day = date('l', strtotime($date));
-          }while(!in_array($next_day, $market_on_days));
+        $date = date("Y-m-d");
+        $date = date('Y-m-d', strtotime(' -1 day'));
         
-        $status = 200;
-        $response = [
-            "msg" => "Result date fetched successfully",
-            "date" => $date
-        ];
+    } else {
+        //taking previous result date and morkaet in days from market table
+        $info = $query->fetchAll(PDO::FETCH_ASSOC)[0];
+        $market_on_days = $info['market_on_days'];
+        $market_on_days = explode(",", $market_on_days);
 
-
-    }else{
-        $status = 203;
-        $response = [
-            "msg" => "No Results Fetched"
-        ];
+        $date = $info['date'];
     }
-    
 
+    //finding next available date
+    do{
+        $date = date('Y-m-d', strtotime($date . '+1 day'));
+        $next_day = date('l', strtotime($date));
+      }while(!in_array($next_day, $market_on_days));
+    
+    $status = 200;
+    $response = [
+        "msg" => "Result date fetched successfully",
+        "date" => $date
+    ];
 }
